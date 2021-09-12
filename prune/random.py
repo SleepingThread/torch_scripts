02e65c89ec6fc9b44c123d46d939cc7b052a1bae
SleepingThread <bellonin_kirill@mail.ru>
2021-09-12 03:13:35 +0300

    prune.random: add RandomPruner

---
+++ prune/random.py
@@ -0,0 +1,22 @@
+import numpy as np
+
+import torch
+
+from .magnitude import Pruner
+
+
+class RandomPruner(Pruner):
+    def prune(self):
+        masks = self.get_masks()
+        with torch.no_grad():
+            shifts = [0]
+            for _m in masks:
+                shifts.append(shifts[-1] + _m.sum().item())
+
+            w_to_prune = np.ones((shifts[-1],), dtype=bool)
+            _inds = np.arange(shifts[-1])
+            np.random.shuffle(_inds)
+            w_to_prune[_inds[:int(self.perc_to_prune * shifts[-1] / 100.)]] = False
+            for _i in range(len(shifts) - 1):
+                _m = masks[_i]
+                _m[_m.clone()] = torch.from_numpy(w_to_prune[shifts[_i]:shifts[_i + 1]])
